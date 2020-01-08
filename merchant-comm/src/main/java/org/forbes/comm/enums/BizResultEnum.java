1385117c97d347777a3fe9b6f9704c343d9a03f9
cfmkaifa <cfmkaifa@shdbr.com>
2020-01-08 13:46:08 +0800

    经营分类管理

--- merchant-comm/src/main/java/org/forbes/comm/enums/BizResultEnum.java
+++ merchant-comm/src/main/java/org/forbes/comm/enums/BizResultEnum.java
@@ -1,84 +1,99 @@
 package org.forbes.comm.enums;
+
 /***
  * BizResultEnum概要说明：业务系统错误代码
  * @author Huanghy
  */
 public enum BizResultEnum {
-	/***
-	 * 001-商家信息
-	 * 功能暂无-表示通用异常
-	 * 001-为空判断
-	 */
-	ENTITY_EMPTY("001001000","对象为空",""),
-	SHOP_STATUS_NO_EXISTS("003001006","商家状态不存在","%s对应商家状态不存在"),
-	REMOTE_ERROR_MSG("003001007","远程调用用户注册异常","远程调用用户注册异常"),
-	ATTCH_DATA_TYPE_ERROR_MSG("003001008","只允许添加商家身份证/法人身份证/营业执照","只允许添加商家身份证/法人身份证/营业执照"),
-	SHOP_EXISTS("003001009","商家已经注册","%s:您已是我们的商家"),
-	SHOP_PHONE_EXISTS("003001010","电话已使用","%s:已使用"),
+    /***
+     * 001-商家信息
+     * 功能暂无-表示通用异常
+     * 001-为空判断
+     */
+    ENTITY_EMPTY("001001000", "对象为空", ""),
+    SHOP_STATUS_NO_EXISTS("003001006", "商家状态不存在", "%s对应商家状态不存在"),
+    REMOTE_ERROR_MSG("003001007", "远程调用用户注册异常", "远程调用用户注册异常"),
+    ATTCH_DATA_TYPE_ERROR_MSG("003001008", "只允许添加商家身份证/法人身份证/营业执照", "只允许添加商家身份证/法人身份证/营业执照"),
+    SHOP_EXISTS("003001009", "商家已经注册", "%s:您已是我们的商家"),
+    SHOP_PHONE_EXISTS("003001010", "电话已使用", "%s:已使用"),
 
-	/**店铺分类**/
-	SHOP_CODE_EXISTS("003002001","店铺分类编码已存在","%s对应店铺分类编码已存在"),
-	SHOP_CASH_STATUS_NO_EXISTS("003002002","商家提现状态不存在","%s对应商家提现状态不存在");
+    /**
+     * 经营分类
+     **/
+    SHOP_CODE_EXISTS("003002001", "经营分类编码已存在", "%s对应经营分类编码已存在"),
+    CLASSIFY_CODE_EXISTS("003002002", "经营分类编码已存在", "%s对应经营分类编码已存在"),
+    CLASSIFY_TYPE_ID_EXISTS("003002003", "经营分类下存在其他商家", "%s对应经营分类下存在其他商家"),
 
-	
-	/**错误编码业务系统代码+功能编码+错误代码**/
-	private String bizCode;
-	/**错误描述****/
-	private String bizMessage;
-	/**带格式错误描述****/
-	private String bizFormateMessage;
+    /**
+     * 商家提现
+     **/
+    SHOP_CASH_STATUS_NO_EXISTS("003003001", "商家提现状态不存在", "%s对应商家提现状态不存在");
 
-	/***
-	 * 构造函数:
-	 * @param bizCode
-	 * @param bizMessage
-	 * @param bizFormateMessage
-	 */
-	BizResultEnum(String bizCode, String bizMessage, String bizFormateMessage){
-		this.bizCode = bizCode;
-		this.bizMessage = bizMessage;
-		this.bizFormateMessage = bizFormateMessage;
-	}
 
-	/** 
-	 * @return bizCode 
-	 */
-	public String getBizCode() {
-		return bizCode;
-	}
+    /**
+     * 错误编码业务系统代码+功能编码+错误代码
+     **/
+    private String bizCode;
+    /**
+     * 错误描述
+     ****/
+    private String bizMessage;
+    /**
+     * 带格式错误描述
+     ****/
+    private String bizFormateMessage;
 
-	/** 
-	 * @param bizCode 要设置的 bizCode 
-	 */
-	public void setBizCode(String bizCode) {
-		this.bizCode = bizCode;
-	}
+    /***
+     * 构造函数:
+     * @param bizCode
+     * @param bizMessage
+     * @param bizFormateMessage
+     */
+    BizResultEnum(String bizCode, String bizMessage, String bizFormateMessage) {
+        this.bizCode = bizCode;
+        this.bizMessage = bizMessage;
+        this.bizFormateMessage = bizFormateMessage;
+    }
 
-	/** 
-	 * @return bizMessage 
-	 */
-	public String getBizMessage() {
-		return bizMessage;
-	}
+    /**
+     * @return bizCode
+     */
+    public String getBizCode() {
+        return bizCode;
+    }
 
-	/** 
-	 * @param bizMessage 要设置的 bizMessage 
-	 */
-	public void setBizMessage(String bizMessage) {
-		this.bizMessage = bizMessage;
-	}
+    /**
+     * @param bizCode 要设置的 bizCode
+     */
+    public void setBizCode(String bizCode) {
+        this.bizCode = bizCode;
+    }
 
-	/** 
-	 * @return bizFormateMessage 
-	 */
-	public String getBizFormateMessage() {
-		return bizFormateMessage;
-	}
+    /**
+     * @return bizMessage
+     */
+    public String getBizMessage() {
+        return bizMessage;
+    }
 
-	/** 
-	 * @param bizFormateMessage 要设置的 bizFormateMessage 
-	 */
-	public void setBizFormateMessage(String bizFormateMessage) {
-		this.bizFormateMessage = bizFormateMessage;
-	}
+    /**
+     * @param bizMessage 要设置的 bizMessage
+     */
+    public void setBizMessage(String bizMessage) {
+        this.bizMessage = bizMessage;
+    }
+
+    /**
+     * @return bizFormateMessage
+     */
+    public String getBizFormateMessage() {
+        return bizFormateMessage;
+    }
+
+    /**
+     * @param bizFormateMessage 要设置的 bizFormateMessage
+     */
+    public void setBizFormateMessage(String bizFormateMessage) {
+        this.bizFormateMessage = bizFormateMessage;
+    }
 }
